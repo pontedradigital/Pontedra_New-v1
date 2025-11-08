cde7984137cc798822bbbfd55cbcba923600167a
[dyad] <pontedradigital@gmail.com>
2025-11-08 16:27:55 -0300

    [dyad] wrote 3 file(s), added sonner package(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"; // Importação já existente
+import { Toaster } from 'sonner'; // Importação adicionada
 import Index from "@/pages/Index";
 import Blog from "@/pages/Blog";
 import BlogPostPage from "@/pages/BlogPostPage";
 import PoliticaPrivacidade from "@/pages/PoliticaPrivacidade";
 import TermosUso from "@/pages/TermosUso";
 import LandingPage from "./pages/LandingPage";
-import Login from "@/pages/Login"; // Importando o novo componente Login
-import Cadastro from "@/pages/Cadastro"; // Importando o novo componente Cadastro
+import Login from "@/pages/Login";
+import Cadastro from "@/pages/Cadastro";
 import NotFound from "./pages/NotFound";
 import ScrollToTopSpecific from "./components/ScrollToTopSpecific";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+      <Toaster position="top-right" richColors /> {/* Toaster adicionado aqui */}
       <ScrollToTopSpecific />
       <Routes>
         {/* Rotas Públicas */}
         <Route path="/" element={<Index />} />
         <Route path="/landing" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} /> {/* Nova rota para Login */}
-        <Route path="/cadastro" element={<Cadastro />} /> {/* Nova rota para Cadastro */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/blog/:slug" element={<BlogPostPage />} />
         <Route path="/politica-privacidade" element={<PoliticaPrivacidade />} />
@@ -28,7 +31,7 @@ function App() {
         {/* Rota 404 */}
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
