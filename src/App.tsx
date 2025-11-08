9d049f0515b9adc80a900b9d70d6383dc9781278
[dyad] <pontedradigital@gmail.com>
2025-11-08 02:14:45 -0300

    [dyad] Removido o componente Router redundante de App.tsx para resolver o erro de Router aninhado. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // Removido BrowserRouter as Router
 import Index from "@/pages/Index";
 import Blog from "@/pages/Blog";
-import BlogPostPage from "@/pages/BlogPostPage"; // Corrigido o nome do componente
+import BlogPostPage from "@/pages/BlogPostPage";
 import PoliticaPrivacidade from "@/pages/PoliticaPrivacidade";
 import TermosUso from "@/pages/TermosUso";
-import LandingPage from "./pages/LandingPage"; // Adicionado para a rota /landing
-import Login from "./pages/login/Login"; // Adicionado para a rota /login
-import Register from "./pages/register/Register"; // Adicionado para a rota /register
-import NotFound from "./pages/NotFound"; // Adicionado para a rota 404
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/landing" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:slug" element={<BlogPostPage />} /> {/* Usando BlogPostPage */}
-        <Route path="/politica-privacidade" element={<PoliticaPrivacidade />} />
-        <Route path="/termos-uso" element={<TermosUso />} />
-        <Route path="*" element={<NotFound />} /> {/* Rota para 404 */}
-      </Routes>
-    </Router>
+    // O Router já é fornecido em src/main.tsx, então removemos daqui.
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/landing" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/blog/:slug" element={<BlogPostPage />} />
+      <Route path="/politica-privacidade" element={<PoliticaPrivacidade />} />
+      <Route path="/termos-uso" element={<TermosUso />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
